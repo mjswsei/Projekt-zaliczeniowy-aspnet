a76c77af5a7b22880a4b3246454f78127c2c9e4c
Maciek <Maciek@KOMP-FAJEN>
2025-01-07 15:27:57 +0100

    Dodaj pliki projektów.

---
+++ Projekt zaliczeniowy aspnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt_zaliczeniowy_aspnet.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
